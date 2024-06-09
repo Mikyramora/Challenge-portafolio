8cf61e287c8f8ddbc380930f43c0f9cf8ea77ca3
Amy Morales <1189moraeli@gmail.com>
2024-06-08 23:52:43 -0600

    testing direct url

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {tiposDeError, mensajes, customErrors} from "/js/errores.js";
+import {tiposDeError, mensajes, customErrors} from "https://mikyramora.github.io/Challenge-portafolio/js/errores.js";
 
 console.log(location.href);
 
